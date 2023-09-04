5fe74048eb52939566554bb7de4bf081515fd41f
Shelton Farias <sheltonm10@outlook.com>
2023-09-04 16:00:27 -0300

    add loop comando for

--- array e loops/script.js
+++ array e loops/script.js
@@ -1,2 +1,8 @@
 var videogames = ['switch', 'ps4', 'xbox'];
- console.log(videogames)
+//  console.log(videogames)
+
+ var ultimoitem = videogames.pop();
+
+for (var numero = 0; numero < 10; numero++ ) {
+    console.log(numero);
+}
